e4846cc171ba573549a8791c59be8c30f5d13b3f
cybardev <50134239+cybardev@users.noreply.github.com>
2025-03-24 00:43:55 -0300

    add node packages to PATH

--- shell.nix
+++ shell.nix
@@ -6,11 +6,9 @@
   ...
 }:
 pkgs.mkShellNoCC {
-  packages = with pkgs; [
-    nodejs_18
-  ];
-
+  packages = [ pkgs.nodejs_18 ];
   shellHook = ''
     npm install
+    export PATH="$PATH:$PWD/node_modules/.bin"
   '';
 }
